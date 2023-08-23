c5940b9417118c1deed03625ae44161607d60fe4
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-08-23 15:52:23 +0530

    new

---
+++ index2.js
@@ -0,0 +1,29 @@
+import express from "express"
+import morgan from "morgan"
+
+const app = express()
+const port = 3001
+
+app.use(morgan("combined"))
+app.use(morgan("tiny"))  //this will log on the console
+
+function getRequest(req,res){
+    res.send("hello")
+}
+
+/*
+default method to use a middleware
+
+appp.use((req,res,next)=>{
+    console.log("Request method: ", req.method)
+    next()
+})
+
+
+*/
+
+app.get('/', getRequest)
+
+app.listen(port , ()=>{
+    console.log('app is listening on port '+port)
+})
